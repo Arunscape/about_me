fb33c5207cd1a2898210587234128f4d387c8ea6
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-03-14 21:32:46 -0600

    closer to what I want

--- src/components/AboutCard.vue
+++ src/components/AboutCard.vue
@@ -3,35 +3,42 @@
   <!-- <v-flex xs12 sm6 offset-sm3> -->
   <v-card>
     <v-container grid-list-md>
-      <v-layout column>
-        <v-flex>
-          <v-avatar size="128">
-            <v-img src="https://avatars1.githubusercontent.com/u/8227297"></v-img>
-          </v-avatar>
+      <v-layout row>
+        <v-flex xs3 text-xs-center>
+          <v-layout column>
+            <v-flex>
+              <v-avatar size="128" align-center justify-center>
+                <v-img src="https://avatars1.githubusercontent.com/u/8227297"></v-img>
+              </v-avatar>
+            </v-flex>
+            <v-flex>
+              <v-btn flat icon>
+                <v-icon>fab fa-github</v-icon>
+              </v-btn>
+              <v-btn flat icon>
+                <v-icon>fab fa-linkedin-in</v-icon>
+              </v-btn>
+              <v-btn flat icon>
+                <v-icon>far fa-envelope</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
         </v-flex>
-        <v-flex>
-          <v-btn flat icon>
-            <v-icon>fab fa-github</v-icon>
-          </v-btn>
-          <v-btn flat icon>
-            <v-icon>fab fa-linkedin-in</v-icon>
-          </v-btn>
-          <v-btn flat icon>
-            <v-icon>far fa-envelope</v-icon>
-          </v-btn>
-        </v-flex>
-      </v-layout>
-      <v-layout column>
-        <v-flex>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatum illum non a aut nulla, incidunt dicta, corrupti blanditiis aspernatur accusantium laborum minima laboriosam, obcaecati dolorum rem sapiente. Dignissimos, voluptate.</v-flex>
-        <v-flex>
-          <v-btn flat>
-            <v-icon class="mr-2">far fa-file-alt</v-icon>
-            <span>Résumé</span>
-          </v-btn>
-          <v-btn flat>
-            <v-icon class="mr-2">far fa-folder-open</v-icon>
-            <span>Projects</span>
-          </v-btn>
+        <v-flex xs9 text-xs-center text-xs-bottom>
+          <v-layout column justify-space-between fill-height>
+            <v-flex>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, reprehenderit nostrum modi officiis minus aliquid inventore voluptatem quis? Cum velit sed dignissimos ipsa adipisci reprehenderit accusamus! Obcaecati mollitia esse cumque.</v-flex>
+            <v-spacer/>
+            <v-flex>
+              <v-btn flat>
+                <v-icon class="mr-2">far fa-file-alt</v-icon>
+                <span>Résumé</span>
+              </v-btn>
+              <v-btn flat>
+                <v-icon class="mr-2">far fa-folder-open</v-icon>
+                <span>Projects</span>
+              </v-btn>
+            </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
