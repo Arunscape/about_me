8375b169f68fdf481d85140fde81a9463e6b9e49
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-04-03 23:16:46 -0600

    nice

---
+++ src/views/Resume.vue
@@ -0,0 +1,13 @@
+<template>
+  <pdf src="https://raw.githubusercontent.com/Arunscape/resume/master/resume.pdf"/>
+</template>
+
+<script>
+import pdf from "vue-pdf";
+
+export default {
+  components: {
+    pdf
+  }
+};
+</script>
