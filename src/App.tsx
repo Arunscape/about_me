2dca9af9fd110b023381be52c76978206c72771a
Arunscape <arun@woosaree.xyz>
2019-05-06 21:17:51 -0600

    add router and change app to a functional component because we now have hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 // import Button from 'antd/lib/button';
 import {Button} from 'antd'
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Button type="primary">Button</Button>
-      </>
-    );
-  }
+
+const App = () => {
+  return (
+    <>
+      <h2>
+        Header
+      </h2>
+      <Router>       
+          <Route path="/" exact component={Index} />
+          <Route path="/products/:id" component={Product} />
+      </Router>
+    </>
+  );
 }
 
+
+const Index = () => <> <p>hello</p> </>
+const Product = () => <> <p>product</p> </>
 export default App;
