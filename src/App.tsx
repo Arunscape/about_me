cdafefd223f3fbad19073e628fa9c3821a5aa707
Arunscape <arun@woosaree.xyz>
2019-05-07 22:53:08 -0600

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 // import Button from 'antd/lib/button';
 import {Button} from 'antd'
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-// const AboutPage = React.lazy(() => import('./pages/AboutPage/AboutPage'));
 
-import AboutPage from './pages/AboutPage/AboutPage'
+const AboutPage = React.lazy(() => import('./pages/AboutPage/AboutPage'));
+const Header = React.lazy(()=> import('./components/Header/Header'));
+
+// import AboutPage from './pages/AboutPage/AboutPage'
 
 
 const App = () => {
   return (
-    <>
-      <h2>
-        Header
-      </h2>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Header/>>
       <Router>       
           <Route path="/" exact component={AboutPage} />
       </Router>
-    </>
+    </Suspense>
   );
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PageHeader } from 'antd';
+
+
+export default () => (
+<PageHeader
+    // onBack={() => null}
+    title="About Me"
+    subTitle="Arun Woosaree"
+  />
+);
+  
