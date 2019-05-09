9d5074be71e59daaff25da3cac14a6fdf7938e8a
Arunscape <arun@woosaree.xyz>
2019-05-08 22:14:33 -0600

    redirects

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { Suspense } from 'react';
 import {Button} from 'antd'
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { tsExpressionWithTypeArguments } from '@babel/types';
 
 
 const AboutPage = React.lazy(() => import('./pages/AboutPage/AboutPage'));
@@ -10,6 +11,26 @@ const Header = React.lazy(()=> import('./components/Header/Header'));
 
 // import AboutPage from './pages/AboutPage/AboutPage'
 
+const externalRedirect = (path:string, url:string) => (
+  <Route path={path} exact component={()=>{
+    window.location.replace(url);
+    return null
+  }}/>
+);
+
+const redirectRoutes = [
+  ["/github", "https://github.com/Arunscape"],
+  ["/linkedin", "https://linkedin.com/in/arunwoosaree"],
+  ["/email", "mailto:arun@woosaree.xyz"],
+  ["/resume", "https://github.com/Arunscape"],
+]
+
+const test = (x:string) => <div>{x}</div>
+const idk = [
+  ["/a", "google.ca"],
+  ["/b", "gmail.com"],
+  ["/c", "youtube.com"],
+]
 
 const App = () => {
   return (
@@ -17,6 +38,8 @@ const App = () => {
       <Header/>>
       <Router>       
           <Route path="/" exact component={AboutPage} />
+          <Route path="/resume" exact component={()=><div>TODO</div>}/>
+          {redirectRoutes.map((x)=>externalRedirect(x[0],x[1]))}
       </Router>
     </Suspense>
   );
