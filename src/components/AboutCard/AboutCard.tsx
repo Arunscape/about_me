a9e2d6803769e69125cdc750742ec2be6cbeb167
Arunscape <arun@woosaree.xyz>
2019-05-08 21:28:00 -0600

    hey it's not jaw dropping but it doesn't look like absolute garbage

--- src/components/AboutCard/AboutCard.tsx
+++ src/components/AboutCard/AboutCard.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {
-  Skeleton, Switch, Card, Icon, Avatar,
+  Skeleton, Switch, Card, Icon, Avatar, Row, Button
 } from 'antd';
+import { Link } from 'react-router-dom'
 
 const { Meta } = Card;
 
+
+const iconWithLink = (icon:string, link:string) => (
+  <Link to={link}>
+    <Icon type={icon}/>
+  </Link>
+)
 
 
 const AboutCard = () =>{
@@ -13,13 +20,25 @@ const AboutCard = () =>{
     <>
       {/* <Switch checked={!loading} onChange={this.onChange} /> */}
 
-      <Card style={{ width: 300, marginTop: 16 }} loading={false}>
+
+      <Card 
+        style={{ width: 350 }} 
+        loading={false}
+        actions={[
+          iconWithLink("github", "/github"),
+          iconWithLink("linkedin", "/linkedin"),
+          iconWithLink("mail", "/email"),
+          iconWithLink("audit", "/resume"),
+        ]}
+      >
         <Meta
-          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-          title="Card title"
-          description="This is the description"
+          avatar={<Avatar src="https://avatars1.githubusercontent.com/u/8227297" size={100} />}
+          title="Arun Woosaree"
+          description="4th Year Computer Software Engineering Co-Op Student at the University of Alberta"
         />
       </Card>
+
+
       </>
 
     );
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -3,10 +3,22 @@ import AboutCard from '../../components/AboutCard/AboutCard'
 
 const AboutPage = () => {
 
+    const center = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '80vh'
+    }
+
+    // const center = {
+    //     position: 'absolute', left: '50%', top: '50%',
+    //     transform: 'translate(-50%, -50%)'
+    // }
+
     return (
-        <>
-            <AboutCard/>
-        </>
+        <div style={center}>
+            <AboutCard />
+        </div>
     )
 }
 
