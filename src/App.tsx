6697a2a1561b55066cfcfe5a13e77ea7b1301a40
Arunscape <arun@woosaree.xyz>
2019-05-09 20:28:21 -0600

    start over agan but with a more popular library that's not translated from mandarin

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import React, { Suspense } from 'react';
-import {Button} from 'antd'
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
-const AboutPage = React.lazy(() => import('./pages/AboutPage/AboutPage'));
-const Header = React.lazy(()=> import('./components/Header/Header'));
-
-const externalRedirect = (path:string, url:string) => (
-  <Route path={path} exact component={()=>{
-    window.location.replace(url);
-    return null
-  }}/>
-);
-
-const redirectRoutes = [
-  ["/github", "https://github.com/Arunscape"],
-  ["/linkedin", "https://linkedin.com/in/arunwoosaree"],
-  ["/email", "mailto:arun@woosaree.xyz"],
-  ["/resume", "https://github.com/Arunscape"],
-]
-
-export default () => {
+const App: React.FC = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Header/>>
-      <Router>       
-          <Route path="/" exact component={AboutPage} />
-          <Route path="/resume" exact component={()=>(<div>TODO</div>)}/>
-          {redirectRoutes.map((x)=>externalRedirect(x[0],x[1]))}
-      </Router>
-    </Suspense>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
