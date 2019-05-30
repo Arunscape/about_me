77c05df98fe93470313b1389b96cbdf5bae12d5a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-05-29 20:35:08 -0600

    the pdf renders but looks like shit

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume: React.FC = () => {
 
 
   return (
-    <>
-      <div>#TODO</div>
-    </>
+    <div style={{
+      width: "100%"
+    }}>
+      <Document
+          // file="https://github.com/Arunscape/resume/raw/master/Arun_Woosaree_Resume.pdf"
+          // file="https://nbviewer.jupyter.org/github/Arunscape/resume/blob/master/Arun_Woosaree_Resume.pdf"
+          file="https://cors-anywhere.herokuapp.com/https://github.com/Arunscape/resume/raw/master/Arun_Woosaree_Resume.pdf"
+          // onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={1} />
+        </Document>
+    </div>
   );
 }
 
