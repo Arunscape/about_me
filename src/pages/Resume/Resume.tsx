473b40d3f697a4c8312525d452d1999a5d1431ac
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-06-03 20:01:13 -0600

    this library looks more promising

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React,{ useRef } from 'react'
+import { usePdf } from 'react-pdf-js';
 
-import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// const PDF_URL = 'https://github.com/Arunscape/resume/raw/master/Arun_Woosaree_Resume.pdf'
+// const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
+const PDF_URL = 'https://raw.githubusercontent.com/Arunscape/resume/master/Arun_Woosaree_Resume.pdf'
 
 const Resume: React.FC = () => {
 
 
+  const canvasEl = useRef(null);
+
+  const [loading, numPages] = usePdf({
+    file: PDF_URL,
+    canvasEl,
+    scale: 3
+  });
+
+  // const scale = window.inner / viewport.width;
+
+
   return (
-    <div style={{
-      width: "100%"
-    }}>
-      <Document
-          // file="https://github.com/Arunscape/resume/raw/master/Arun_Woosaree_Resume.pdf"
-          // file="https://nbviewer.jupyter.org/github/Arunscape/resume/blob/master/Arun_Woosaree_Resume.pdf"
-          file="https://cors-anywhere.herokuapp.com/https://github.com/Arunscape/resume/raw/master/Arun_Woosaree_Resume.pdf"
-          // onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          <Page pageNumber={1} />
-        </Document>
-    </div>
+    <>
+      {loading && <span>Loading...</span>}
+      <canvas 
+        width={window.innerWidth}
+        height={window.innerHeight}
+        ref={canvasEl} 
+      />
+    </>
   );
+
+
 }
 
 export default Resume;
