2bbbb6c4f6d6aa26f8fa87d801659fa497a06553
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-06-12 19:59:32 -0600

    nice, we can load the projects as an array

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const PROJECTS_URL = 'https://raw.githubusercontent.com/Arunscape/About-Me/master/src/data/projects.json';
 
 const Projects: React.FC = () => {
 
+    const [projects, setProjects] = useState(null)
+    useEffect(()=>{
+        fetch(PROJECTS_URL)
+        .then(res => res.json())
+        .then(data => setProjects(data))
+    }, [])
 
+    console.log(projects)
     return (
-        <div>
-            #TODO
-        </div>
+        <>
+        {projects ? (
+            <div>Stuff</div>
+        ) : (<div>Loading...</div>)
+        }
+        </>
     )
-}
+    }
+    
+
 
 export default Projects;
