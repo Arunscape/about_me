49111ec81e335213d084f1bb656e38a0c0132434
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-08-22 19:37:45 -0600

    nice

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,22 +1,32 @@
 import React, { Suspense } from 'react';
+import Paper from '@material-ui/core/Paper'
+
 const AboutCard = React.lazy(() => import('../../components/AboutCard/AboutCard'))
 
 
 const AboutPage: React.FC = () => {
 
-    const center = {
+    const style = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        height: '80vh'
+        height: '100%',
+        width: '100%',
+        position: 'fixed',
+        backgroundImage: "url('https://raw.githubusercontent.com/Arunscape/About-Me/gh-pages/images/bg3.jpg')",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
     }
 
     return (
-        <div style={center}>
+        // @ts-ignore
+        <Paper style={style}>
             <Suspense fallback={<div>Loading...</div>}>
                 <AboutCard />
             </Suspense>
-        </div>
+        </Paper>
     )
 }
 
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -8,6 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper'
 
 import IconButton from '@material-ui/core/IconButton'
 import GithubCircle from 'mdi-material-ui/GithubCircle'
@@ -35,10 +36,26 @@ const useStyles = makeStyles(
       flexGrow: 1,
       width: '95%',
       margin: "0 auto",
-      marginTop: '1%',
+      marginTop: '2%',
     },
   }),
 );
+
+const style = {
+  // display: 'flex',
+  // alignItems: 'center',
+  // justifyContent: 'center',
+  height: '100%',
+  width: '100%',
+  position: 'fixed',
+  backgroundImage: "url('https://raw.githubusercontent.com/Arunscape/About-Me/gh-pages/images/bg3.jpg')",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center center",
+  backgroundSize: "cover",
+  backgroundAttachment: "fixed",
+  filter: "blur(8px)",
+  zIndex: -1,
+}
 
 const PROJECTS_URL = 'https://raw.githubusercontent.com/Arunscape/About-Me/master/src/data/projects.json';
 
@@ -61,79 +78,83 @@ const Projects: React.FC = () => {
 
 
   return (
-    <div
-      className={classes.root}
-    >
-      <Grid
-        container
-        direction="row"
-        justify="space-around"
-        alignItems="flex-start"
-        spacing={3}
-      >
-        {projects.map(p => (
-          <Grid
-            item
-            // xl
-            key={p.title}
-          >
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt=""
-                  height="140"
-                  image={p.previewimage}
-                  title={p.title}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {p.title}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    {p.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
+    <>
+      <Paper
+        // @ts-ignore
+        style={style}
+      />
 
-                {p.demourl && (
-                  <Button
-                    size="small"
-                    color="primary"
-                    onClick={() => openNewTab(p.demourl!)}
-                  >
-                    Demo
+      <div className={classes.root}>
+        <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="flex-start"
+          spacing={3}
+        >
+          {projects.map(p => (
+            <Grid
+              item
+              // xl
+              key={p.title}
+            >
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt=""
+                    height="140"
+                    image={p.previewimage}
+                    title={p.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {p.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {p.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+
+                  {p.demourl && (
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={() => openNewTab(p.demourl!)}
+                    >
+                      Demo
                   </Button>
-                )}
+                  )}
 
-                {p.githuburl && (
-                  <IconButton
-                    aria-label="View the souce code on GitHub"
-                    onClick={() => openNewTab(p.githuburl!)}
-                  >
-                    <GithubCircle />
-                  </IconButton>
-                )}
+                  {p.githuburl && (
+                    <IconButton
+                      aria-label="View the souce code on GitHub"
+                      onClick={() => openNewTab(p.githuburl!)}
+                    >
+                      <GithubCircle />
+                    </IconButton>
+                  )}
 
-                {p.moreinfo && (
-                  <Button
-                    size="small"
-                    color="primary"
-                    onClick={() => openNewTab(p.moreinfo!)}
-                  >
-                    More Info
+                  {p.moreinfo && (
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={() => openNewTab(p.moreinfo!)}
+                    >
+                      More Info
                   </Button>
-                )}
+                  )}
 
 
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </>
   )
 }
 
