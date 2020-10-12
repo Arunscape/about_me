0f62a5c6eccb9d57b5a023a9674e61d2e25c0e1f
Arun Woosaree <arun@woosaree.xyz>
2020-10-12 07:38:10 +0000

    oof

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Document, Page } from 'react-pdf';
 
 
-import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 // @ts-ignore
 import { pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
