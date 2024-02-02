20e2c5d8670e619883e8f59dbc7f62702e85d223
Arunscape <8227297+Arunscape@users.noreply.github.com>
2024-02-01 18:34:46 -0700

    aaaaa

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -3,6 +3,7 @@ use crate::error_template::{AppError, ErrorTemplate};
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use memoize::memoize;
 
 pub mod error_template;
 
@@ -13,9 +14,13 @@ pub fn App() -> impl IntoView {
 
     view! {
         <Stylesheet id="leptos" href="/pkg/start-axum-workspace.css"/>
+        <Html attr:data-theme="catppuccin-mocha"/>
+  //      <Html attr:data-theme="forest"/>
+        <Link rel="shortcut icon" href="/images/arun.svg"/>
+        <Meta name="description" content="Arun's Website"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Arun's Website"/>
 
         // content for this welcome page
         <Router fallback=|| {
@@ -26,6 +31,8 @@ pub fn App() -> impl IntoView {
             <main>
                 <Routes>
                     <Route path="" view=HomePage/>
+                    <Route path="/blog" view=BlogList/>
+                    <Route path="/blog/:id" view=BlogPost/>
                 </Routes>
             </main>
         </Router>
@@ -41,6 +48,63 @@ fn HomePage() -> impl IntoView {
 
     view! {
         <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h2>"You clicked " {count} " times"</h2>
+        <button class="btn btn-primary" on:click=on_click>
+            "Click Me: "
+            {count}
+        </button>
     }
+}
+
+fn BlogList() -> impl IntoView {
+    view! { <h1>"Blog List"</h1> }
+}
+
+
+
+#[derive(Params, PartialEq, Default)]
+struct BlogPostParams {
+    id: String,
+}
+
+#[component]
+fn BlogPost() -> impl IntoView {
+
+    let params = use_params::<BlogPostParams>();
+
+    let id = move || {
+        params.with(|p| {
+            p.as_ref().map(|p| p.id.clone()).unwrap_or_default()
+        })
+    };
+
+    let once = create_resource(||(), move |_| async move {
+        get_blog(id()).await
+    });
+
+
+    view! {
+        <>
+            <Title text=id/>
+            <Meta name="description" content=id/>
+            <h1>{format!("{}", id())}</h1>
+            <div>{once.get()}</div>
+        </>
+    }
+}
+
+#[memoize]
+fn get_blog_html(name: String) -> String {
+    todo!()
+}
+
+#[server]
+async fn get_blog(id: String) -> Result<String, ServerFnError> {
+    use comrak::markdown_to_html;
+
+
+    let md = "";
+    let html = markdown_to_html(&md, &comrak::ComrakOptions::default());
+
+    Ok(html)
 }
